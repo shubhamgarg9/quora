0cefd94983f918890bc1e4dc5bc853697e78ac62
shubhamgarg9 <shubhamrad9@gmail.com>
2017-10-14 22:40:08 +0530

    new and create added for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,7 +13,21 @@ class QuestionsController < ApplicationController
 	end
 
 	def create
+		@question  = Question.new(question_params)
+		@question.user = User.find(4)
 
+		if @question.save
+			flash[:success] = "your question got created successfully"
+			redirect_to questions_path
+		else
+
+		end
 	end
+
+	private
+
+		def question_params
+			params.require(:question).permit(:text, :description)
+		end
 
 end
