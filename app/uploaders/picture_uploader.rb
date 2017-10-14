ba4df087083ee9523fbcb3b0375636fe29788825
shubhamgarg9 <shubhamrad9@gmail.com>
2017-10-15 01:14:28 +0530

    image-uploader

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,5 +1,7 @@
 class PictureUploader < CarrierWave::Uploader::Base
 
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [600,600]
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
